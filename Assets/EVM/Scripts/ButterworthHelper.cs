d3a642ff8f7c9d0f6423ef293b1a3f1d1b328d9a
shyu216 <yusihong073@gmail.com>
2025-06-21 16:54:11 +1000

    add starter samples from Unity-StarterSamples

---
+++ Assets/EVM/Scripts/ButterworthHelper.cs
@@ -0,0 +1,195 @@
+/*
+ * This code is adapted from MathNet.Filtering, Version=0.7.0.0.
+ *
+ * The original design interface has been removed to directly compute 
+ * numerator and denominator coefficients from cutoff frequency and sampling frequency.
+ *
+ * Author: SIHONG YU
+ * Date: 2025.6
+ */
+
+using System;
+using System.Numerics;
+using MathNet.Filtering.TransferFunctions;
+using MathNet.Numerics;
+
+namespace EVM
+{
+    public static class ButterworthHelper
+    {
+        //
+        // Summary:
+        //     Computes the warped frequency.
+        //
+        // Parameters:
+        //   cutoffFrequency:
+        //     Digital cutoff frequency.
+        //
+        //   samplingTime:
+        //     Sampling time (computed as the reverse of the sampling frequency).
+        //
+        // Returns:
+        //     Warped frequency.
+        private static double WarpFrequency(double cutoffFrequency, double samplingTime)
+        {
+            return Math.Tan(Math.PI * cutoffFrequency / samplingTime);
+        }
+
+        //
+        // Summary:
+        //     Computes the coefficients of the polynomial whose solutions are roots are given
+        //     as input parameter.
+        //
+        // Parameters:
+        //   roots:
+        //     Roots of the polynomial.
+        //
+        // Returns:
+        //     Polynomial coefficients.
+        private static Complex[] PolynomialCoefficients(Complex[] roots)
+        {
+            Complex[] array = Generate.Repeat(roots.Length + 1, Complex.Zero);
+            array[0] = Complex.One;
+            for (int i = 0; i < roots.Length; i++)
+            {
+                for (int num = i; num >= 0; num--)
+                {
+                    array[num + 1] -= roots[i] * array[num];
+                }
+            }
+
+            return array;
+        }
+
+        //
+        // Summary:
+        //     Recomputes the gain and the list of zeros and poles for a low-pass filter.
+        //
+        // Parameters:
+        //   gain:
+        //     Initial gain.
+        //
+        //   zeros:
+        //     List of zeros.
+        //
+        //   poles:
+        //     List of poles.
+        //
+        //   wc:
+        //     Cutoff frequency.
+        //
+        // Returns:
+        //     Recomputed gain, list of zeros and list of poles.
+        //
+        // Comments:
+        //     For performance reasons, the method edits the input list of zeros and poles in
+        //     place, so the input and output arrays are actually the same object.
+        private static (double gain, Complex[] zeros, Complex[] poles) LowPassTransferFunctionTransformer(double gain, Complex[] zeros, Complex[] poles, double wc)
+        {
+            int num = zeros.Length;
+            int num2 = poles.Length;
+            gain *= Math.Pow(1.0 / wc, num - num2);
+            for (int num3 = num - 1; num3 >= 0; num3--)
+            {
+                zeros[num3] *= (Complex)wc;
+            }
+
+            for (int num4 = num2 - 1; num4 >= 0; num4--)
+            {
+                poles[num4] *= (Complex)wc;
+            }
+
+            return (gain, zeros, poles);
+        }
+
+        //
+        // Summary:
+        //     Computes the IIR coefficients for a low-pass Butterworth filter.
+        //
+        // Parameters:
+        //   passbandFreq:
+        //     Passband corner frequency (in Hz).
+        //
+        //   stopbandFreq:
+        //     Stopband corner frequency (in Hz).
+        //
+        //   passbandRipple:
+        //     Maximum allowed passband ripple.
+        //
+        //   stopbandAttenuation:
+        //     Minimum required stopband attenuation.
+        //
+        // Returns:
+        //     IIR coefficients.
+        public static (double[] numerator, double[] denominator) LowPass(byte n, double wc)
+        {
+            byte item = n;
+            double item2 = wc;
+            (double gain, Complex[] zeros, Complex[] poles) tuple2 = TransferFunction(item);
+            double item3 = tuple2.gain;
+            Complex[] item4 = tuple2.zeros;
+            Complex[] item5 = tuple2.poles;
+            item2 = WarpFrequency(item2, 2.0);
+            (item3, item4, item5) = LowPassTransferFunctionTransformer(item3, item4, item5, item2);
+            return Coefficients(item3, item4, item5, 2.0);
+        }
+
+        //
+        // Summary:
+        //     Computes the transfer function for a generic Butterworth filter.
+        //
+        // Parameters:
+        //   n:
+        //     Order of the filter.
+        //
+        // Returns:
+        //     The triplet gain, zeros and poles of the transfer function
+        private static (double gain, Complex[] zeros, Complex[] poles) TransferFunction(uint n)
+        {
+            Complex[] item = new Complex[0];
+            Complex[] array = new Complex[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Complex.Exp(Complex.ImaginaryOne * (Complex)Math.PI * (Complex)(2 * (i + 1) + n - 1) / (Complex)(2 * n));
+            }
+
+            if ((n & 1) == 1)
+            {
+                uint num = (n + 1) / 2;
+                array[num - 1] = -1;
+            }
+
+            return (1.0, item, array);
+        }
+
+        //
+        // Summary:
+        //     Returns the list of IIR coefficients for a generic Butterworth filter.
+        //
+        // Parameters:
+        //   gain:
+        //     Filter gain.
+        //
+        //   zeros:
+        //     Filter zeros list.
+        //
+        //   poles:
+        //     Filter poles list.
+        //
+        //   T:
+        //     Sampling time (inverse of sampling frequency).
+        //
+        // Returns:
+        //     The list of IIR coefficients.
+        private static (double[] numerator, double[] denominator) Coefficients(double gain, Complex[] zeros, Complex[] poles, double T)
+        {
+            (double, Complex[], Complex[]) tuple = BilinearTransform.Apply(gain, zeros, poles, T);
+            gain = tuple.Item1;
+            zeros = tuple.Item2;
+            poles = tuple.Item3;
+            double[] item = Generate.Map(PolynomialCoefficients(zeros), (Complex num) => (num * (Complex)gain).Real);
+            double[] item2 = Generate.Map(PolynomialCoefficients(poles), (Complex den) => den.Real);
+            return (item, item2);
+        }
+    }
+}
---
+++ Assets/StarterSamples/Usage/Passthrough/Scripts/EnableDisablePassthroughController.cs
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * Licensed under the Oculus SDK License Agreement (the "License");
+ * you may not use the Oculus SDK except in compliance with the License,
+ * which is provided at the time of installation or download, or which
+ * otherwise accompanies this software in either electronic or hard copy form.
+ *
+ * You may obtain a copy of the License at
+ *
+ * https://developer.oculus.com/licenses/oculussdk/
+ *
+ * Unless required by applicable law or agreed to in writing, the Oculus SDK
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections;
+using Meta.XR.Samples;
+using UnityEngine;
+using UnityEngine.UI;
+
+[MetaCodeSample("StarterSample-Passthrough")]
+public class EnableDisablePassthroughController : MonoBehaviour
+{
+    private static readonly int InvertedAlpha = Shader.PropertyToID("_InvertedAlpha");
+
+    [SerializeField]
+    [Tooltip("The passthrough layer to which we will subscribe for the resumed event")]
+    private OVRPassthroughLayer passthroughLayer;
+
+    [SerializeField]
+    [Tooltip("The mesh renderer on which the fading effect is applied")]
+    private MeshRenderer passthroughRenderer;
+
+    [SerializeField]
+    [Tooltip("The UI toggle that enables the event subscription")]
+    private Toggle passthroughResumedToggle;
+
+    [SerializeField]
+    [Tooltip("The speed of the passthrough transition effect. Value N means the transition happens in 1/N of a sec")]
+    private float passthroughFadeSpeed = 5f;
+
+    private Material _material;
+    private PassthroughTransitionType lastTransitionRequested;
+    private Coroutine transitionCoroutine;
+
+    private void Awake()
+    {
+        OVRManager.eyeFovPremultipliedAlphaModeEnabled = false;
+        _material = passthroughRenderer.material;
+    }
+
+    private void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            TogglePassthrough();
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (passthroughResumedToggle.isOn)
+        {
+            passthroughLayer.passthroughLayerResumed.AddListener(OnPassthroughLayerResumed);
+        }
+    }
+
+    private void OnDisable()
+    {
+        passthroughLayer.passthroughLayerResumed.RemoveListener(OnPassthroughLayerResumed);
+    }
+
+    private void TogglePassthrough()
+    {
+        PassthroughTransitionType transitionRequested = lastTransitionRequested == PassthroughTransitionType.MrToVr
+            ? PassthroughTransitionType.VrToMr
+            : PassthroughTransitionType.MrToVr;
+
+        if (transitionCoroutine != null)
+        {
+            StopCoroutine(transitionCoroutine);
+        }
+
+        transitionCoroutine = transitionRequested == PassthroughTransitionType.MrToVr
+            ? StartCoroutine(TransitionToVR())
+            : StartCoroutine(TransitionToMR());
+
+        lastTransitionRequested = transitionRequested;
+    }
+
+    private IEnumerator TransitionToMR()
+    {
+        // Start Passthrough feature
+        OVRManager.instance.isInsightPassthroughEnabled = true;
+        passthroughLayer.enabled = true;
+        passthroughLayer.hidden = false;
+
+        // If we don't use PassthroughResumedEvent event, then we start the transition right here
+        // Otherwise, the transition starts in the event handler.
+        if (!passthroughResumedToggle.isOn)
+        {
+            passthroughRenderer.enabled = true;
+            yield return ChangePassthroughVisibility(targetVisibility: 1f);
+        }
+    }
+
+    private IEnumerator TransitionToVR()
+    {
+        // First, fade passthrough
+        yield return ChangePassthroughVisibility(targetVisibility: 0f);
+
+        // Disable passthrough related GameObjects and stop the feature
+        passthroughRenderer.enabled = false;
+        passthroughLayer.enabled = false;
+        passthroughLayer.hidden = true;
+        OVRManager.instance.isInsightPassthroughEnabled = false;
+    }
+
+    /// <summary>
+    /// Linearly changes the transparency of the background passthrough on the scene.
+    /// 1.0 means passthrough is fully visible, 0.0 means no passthrough
+    /// </summary>
+    private IEnumerator ChangePassthroughVisibility(float targetVisibility)
+    {
+        var currentAlpha = _material.GetFloat(InvertedAlpha);
+        while (!Mathf.Approximately(currentAlpha, targetVisibility))
+        {
+            currentAlpha = Mathf.MoveTowards(currentAlpha, targetVisibility, passthroughFadeSpeed * Time.deltaTime);
+            _material.SetFloat(InvertedAlpha, currentAlpha);
+            yield return null;
+        }
+
+        _material.SetFloat(InvertedAlpha, targetVisibility);
+    }
+
+    private void OnPassthroughLayerResumed(OVRPassthroughLayer layer)
+    {
+        // Enable the passthrough renderer, and start a coroutine which will slowly reveal passthrough
+        passthroughRenderer.enabled = true;
+        if (transitionCoroutine != null)
+        {
+            StopCoroutine(transitionCoroutine);
+        }
+        transitionCoroutine = StartCoroutine(ChangePassthroughVisibility(targetVisibility: 1f));
+    }
+
+    public void TogglePassthroughResumedCallback(bool shouldSubscribe)
+    {
+        if (shouldSubscribe)
+        {
+            passthroughLayer.passthroughLayerResumed.AddListener(OnPassthroughLayerResumed);
+        }
+        else
+        {
+            passthroughLayer.passthroughLayerResumed.RemoveListener(OnPassthroughLayerResumed);
+        }
+    }
+
+    private enum PassthroughTransitionType
+    {
+        VrToMr,
+        MrToVr
+    }
+}
